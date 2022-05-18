e8b79bef6c84d39359eed0ca51789e4653dc3b5e
prateek825 <prateek.singh@bonamisoftware.com>
2022-05-18 18:05:15 +0530

    second commit

--- src/main/java/com/luv2code/springdemo/controller/CustomerController.java
+++ src/main/java/com/luv2code/springdemo/controller/CustomerController.java
@@ -1,15 +1,29 @@
 package com.luv2code.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
-	@RequestMapping("/list")
+	@Autowired
+	private CustomerService customerService;
+	
+	@GetMapping("/list")
 	public String listCustomer(Model theModel) {
+		
+		List<Customer> theCustomers = customerService.getCustomers();
+		
+		theModel.addAttribute("customers",theCustomers);
 		
 		return "list-customers";
 	}
